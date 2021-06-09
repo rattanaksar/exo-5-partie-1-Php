90c5ba2e2038ed73073e4672a07cadcf62cdcb86
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 11:03:20 +0200

    Exo 5 PHP

---
+++ Exo 5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 5 : Les variables PHP</title>
+</head>
+<body>
+    <h1>Exo 5 Partie : Les variables PHP</h1>
+    <?php
+        $a= 4567;
+        $a=789;
+        echo $a;
+    ?>
+</body>
+</html>
